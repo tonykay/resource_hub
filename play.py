97a248bedbbd421f863c203a8c9d46ee093b02c2
Tony <tony.g.kay@gmail.com>
2020-07-16 15:48:40 -0600

    Fixed config preloading of dataset

--- play.py
+++
@@ -1,4 +0,0 @@
-dataList = [{'a': 1}, {'b': 3}, {'c': 5}]
-for dic in dataList:
-        for val in dic.values():
-                    print(val)
---
+++ resources.py
@@ -0,0 +1,10 @@
+resources = [
+        { 'name': 'Ansible for DevOps', 'description': 'Classic introduction to Ansible', 
+        'author': 'That Jeff Geerlinguy', 'source': 'Book' },
+        { 'name': 'Mastering Ansible 4rd Edition', 'description': 'Explores the plumbing', 
+        'author': 'Jeff Keating', 'source': 'Book' },
+        { 'name': 'CloudAssember', 'description': 'Explores the plumbing', 
+        'author': 'Tok', 'source': 'Blog' },
+        { 'name': 'Cloud Stuff', 'description': 'Explores the plumbing', 
+        'author': 'Jeff Keating', 'source': 'Book' }
+        ]
