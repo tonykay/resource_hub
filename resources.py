d53a00ffc418acfe07deacf9d81d353992ada8f5
Tony <tony.g.kay@gmail.com>
2020-07-17 08:58:59 -0600

    Fixes to formatting resources.py

--- resources.py
+++ resources.py
@@ -1,30 +1,30 @@
 resources = [
-        { 
-            'name': 'Ansible for DevOps', 
-            'description': 'Classic introduction to Ansible', 
-            'author': 'That Jeff Geerlinguy',
-            'url': 'https://leanpub.com/ansible-for-devops', 
-            'source': 'Book' 
-        },
-        { 
-            'name': 'Mastering Ansible 3rd Edition', 
-            'description': 'Explores how Ansible works', 
-            'author': 'James Freeman, Jesse Keating', 
-            'url': 'https://www.packtpub.com/virtualization-and-cloud/mastering-ansible-third-edition`', 
-            'source': 'Book' 
-        },
-        { 
-            'name': 'CloudAssember', 
-            'description': "Tony Kay's Ansible centric Blog", 
-            'author': 'Tok', 
-            'url': 'http://www.cloudassembler.com/', 
-            'source': 'Blog' 
-        },
-        { 
-            'name': 'Ansible 2 Cloud Automation Cookbook', 
-            'description': 'Overview of Ansible in common Cloud Environments', 
-            'author': 'Aditya Patawari, Vikas Aggarwal', 
-            'url': 'https://www.packtpub.com/virtualization-and-cloud/ansible-2-cloud-automation-cookbook', 
-            'source': 'Book' 
-        },
-        ]
+    { 
+        'name': 'Ansible for DevOps', 
+        'description': 'Classic introduction to Ansible', 
+        'author': 'That Jeff Geerlinguy',
+        'url': 'https://leanpub.com/ansible-for-devops', 
+        'source': 'Book' 
+    },
+    { 
+        'name': 'Mastering Ansible 3rd Edition', 
+        'description': 'Explores how Ansible works', 
+        'author': 'James Freeman, Jesse Keating', 
+        'url': 'https://www.packtpub.com/virtualization-and-cloud/mastering-ansible-third-edition`', 
+        'source': 'Book' 
+    },
+    { 
+        'name': 'CloudAssember', 
+        'description': "Tony Kay's Ansible centric Blog", 
+        'author': 'Tok', 
+        'url': 'http://www.cloudassembler.com/', 
+        'source': 'Blog' 
+    },
+    { 
+        'name': 'Ansible 2 Cloud Automation Cookbook', 
+        'description': 'Overview of Ansible in common Cloud Environments', 
+        'author': 'Aditya Patawari, Vikas Aggarwal', 
+        'url': 'https://www.packtpub.com/virtualization-and-cloud/ansible-2-cloud-automation-cookbook', 
+        'source': 'Book' 
+    }
+]
